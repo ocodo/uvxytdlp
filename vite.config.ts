0aa3b443abcb3e01e513ebe71b0bd96c064287d7
jasonm23 <jasonm23@gmail.com>
2025-07-06 09:48:08 +0700

    Update gitignore and uv.lock, add note to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,11 @@ import mdx from '@mdx-js/rollup'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    // just localhost, or ip addresses,
+    // see vite docs to config allowed host names
+    allowedHosts: []
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src/'),
