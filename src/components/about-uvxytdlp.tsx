b3a21a5618a8af40f8ea38c2959e7038109ccab0
jasonm23 <jasonm23@gmail.com>
2025-07-13 22:57:16 +0700

    Add version.ts / show in About

--- src/components/about-uvxytdlp.tsx
+++ src/components/about-uvxytdlp.tsx
@@ -1,12 +1,16 @@
 import { YtdlpSupportedSites } from "@/components/bookmarklet-settings-view";
 import { RadixIconsGithubLogo } from "@/components/radix-icons-github-logo";
 import { CopyleftIcon } from "lucide-react";
+import { VERSION } from '@/lib/version';
 import type { FC } from "react";
 
 export const AboutUvxYtdlp: FC = () => (
   <>
-    <div className="font-black tracking-tighter text-5xl mb-4">
+    <div className="font-black tracking-tighter text-5xl">
       uvxytdlp
+    </div>
+    <div className="text-xs mb-5 text-foreground/50">
+      {VERSION}
     </div>
     <div>
       Download videos from <YtdlpSupportedSites title="any yt-dlp compatible site" />
